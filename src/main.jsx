0d4310bbd88d5f5c9e6aef13d307233a79fcc244
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2023-06-20 02:21:38 +0300

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import HomePage from './pages/HomePage.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import UpdatePage from './pages/UpdatePage.jsx';
+import "./main.css"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>
+  },
+  {
+    path: 'update/:id',
+    element: <UpdatePage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className="main-wrapper">
+    <h1 className="font-effect-anaglyph">Контакты</h1>
+    <div className="router">
+    <RouterProvider 
+      router={router}
+    />
+    </div>
+    </div>
+  
+  
   </React.StrictMode>,
 )
